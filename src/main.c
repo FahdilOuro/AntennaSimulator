dbf7e816286697067d47af59d81effd0df9b4beb
Fahdilou_ouro <fahdiloufahd@gmail.com>
2025-01-05 19:54:47 +0100

    premier version de main

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "antenna.h"
+#include "mesh.h"
+#include "solver.h"
+#include "utils.h"
+
+int main() {
+    // Initialisation
+    Antenna dipole = create_antenna_from_file("data/antenna_config.txt");
+    Mesh mesh = generate_mesh(dipole);
+
+    // Resolution
+    double* currents = solve_mom(mesh, dipole);
+
+    // Resultats
+    print_currents(currents, mesh.num_elements);
+
+    // Nettoyage
+    free_mesh(mesh);
+    free(currents);
+}
